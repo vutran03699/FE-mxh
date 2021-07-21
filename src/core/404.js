9c0a9d3796d4f4dd1508068ef91b678a6819db9d
Asland <huynhnhuth53@gmail.com>
2021-07-21 21:59:03 +0700

    fix main home

--- src/core/404.js
+++ src/core/404.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import notfoundpage from '../css/notfoundpage.css'
 
-const NotFounh =()=>(
+const NotFounh = () => (
     <div>
-        <h1>404</h1>
+        <div className="container">
+            <h1 className="first-four">4</h1>
+            <div className="cog-wheel1">
+                <div className="cog1">
+                    <div className="top" />
+                    <div className="down" />
+                    <div className="left-top" />
+                    <div className="left-down" />
+                    <div className="right-top" />
+                    <div className="right-down" />
+                    <div className="left" />
+                    <div className="right" />
+                </div>
+            </div>
+            <div className="cog-wheel2">
+                <div className="cog2">
+                    <div className="top" />
+                    <div className="down" />
+                    <div className="left-top" />
+                    <div className="left-down" />
+                    <div className="right-top" />
+                    <div className="right-down" />
+                    <div className="left" />
+                    <div className="right" />
+                </div>
+            </div>
+            <h1 className="second-four">4</h1>
+            <p className="wrong-para">Uh Oh! Page not found!</p>
+        </div>
+
         <Link to="/">Go Home</Link>
     </div>
 );
